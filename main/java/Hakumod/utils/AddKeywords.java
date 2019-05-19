2b934e8e3879626ab0fa4603287b32842eb4f19d
Rita-Bernstein <48193905+Rita-Bernstein@users.noreply.github.com>
2019-05-20 02:10:07 +0800

    for localzation

--- main/java/Hakumod/utils/AddKeywords.java
+++ main/java/Hakumod/utils/AddKeywords.java
@@ -6,13 +6,35 @@ import com.google.gson.Gson;
 
 import java.nio.charset.StandardCharsets;
 
+import com.megacrit.cardcrawl.core.Settings;
+
 public class AddKeywords {
 
     AddKeywords(){}
 
     public static void addKeywords(){
+        String language;
+        switch (Settings.language) {
+            // case KOR:
+            //     language = "kor";
+            //     break;
+            case ZHS:
+                language = "zhs";
+                break;
+            // case ZHT:
+            //     language = "zht";
+            //     break;
+            // case FRA:
+            //     language = "fra";
+            //     break;
+            // case JPN:
+            //     language = "jpn";
+            //     break;
+            default:
+                language = "eng";
+        }
         Gson gson = new Gson();
-        String json = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Keywords.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        String json = Gdx.files.internal(AssetsPath.localizationPath( language +"/Hakumod_Keywords.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         Keyword[] keywords = gson.fromJson(json, Keyword[].class);
 
         if (keywords != null) {
--- main/java/Hakumod/utils/EditStrings.java
+++ main/java/Hakumod/utils/EditStrings.java
@@ -6,34 +6,56 @@ import com.megacrit.cardcrawl.localization.*;
 
 import java.nio.charset.StandardCharsets;
 
+import com.megacrit.cardcrawl.core.Settings;
+
 public class EditStrings {
     EditStrings(){ }
 
     public static void editStrings(){
+        String language;
+        switch (Settings.language) {
+            // case KOR:
+            //     language = "kor";
+            //     break;
+              case ZHS:
+                language = "zhs";
+                 break;
+            // case ZHT:
+            //     language = "zht";
+            //     break;
+            // case FRA:
+            //     language = "fra";
+            //     break;
+            // case JPN:
+            //     language = "jpn";
+            //     break;
+            default:
+                language = "eng";
+        }
         String relicStrings,cardStrings,powerStrings, potionStrings, monsterStrings, orbStrings, uiStrings, characterStrings;
 
-        relicStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Relics.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        relicStrings = Gdx.files.internal(AssetsPath.localizationPath( language + "/Hakumod_Relics.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(RelicStrings.class, relicStrings);
 
-        cardStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Cards.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        cardStrings = Gdx.files.internal(AssetsPath.localizationPath( language +"/Hakumod_Cards.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(CardStrings.class, cardStrings);
 
-        powerStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Powers.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        powerStrings = Gdx.files.internal(AssetsPath.localizationPath( language +"/Hakumod_Powers.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(PowerStrings.class, powerStrings);
 
-        potionStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Potions.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        potionStrings = Gdx.files.internal(AssetsPath.localizationPath( language +"/Hakumod_Potions.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(PotionStrings.class, potionStrings);
 
-        orbStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Orbs.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        orbStrings = Gdx.files.internal(AssetsPath.localizationPath(language +"/Hakumod_Orbs.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(OrbStrings.class, orbStrings);
 
-        monsterStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Monsters.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        monsterStrings = Gdx.files.internal(AssetsPath.localizationPath( language +"/Hakumod_Monsters.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(MonsterStrings.class, monsterStrings);
 
-        uiStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_UI.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        uiStrings = Gdx.files.internal(AssetsPath.localizationPath(language +"/Hakumod_UI.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(UIStrings.class, uiStrings);
 
-        characterStrings = Gdx.files.internal(AssetsPath.localizationPath("Hakumod_Character.json")).readString(String.valueOf(StandardCharsets.UTF_8));
+        characterStrings = Gdx.files.internal(AssetsPath.localizationPath( language +"/Hakumod_Character.json")).readString(String.valueOf(StandardCharsets.UTF_8));
         BaseMod.loadCustomStrings(CharacterStrings.class, characterStrings);
     }
 }
